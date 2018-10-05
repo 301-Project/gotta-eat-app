ed5cbe6e68c83b50571f8c91a34fb6a9d23d53c5
dhwang21 <hwang.derrick@gmail.com>
2018-10-05 11:22:02 -0700

    merging to solve a bug

--- public/app.js
+++ public/app.js
@@ -4,13 +4,19 @@ console.log('in app.js');
 
 let mainNav = document.getElementById('js-navigation');
 let bookmark = document.getElementById('js-bookmark');
+let recipe = document.getElementById('js-recipe');
+
 
 let navBarToggle = document.getElementById('js-navbar-toggle');
 let bookmarkToggle = document.getElementById('js-bookmark-toggle');
+let recipeToggle = document.getElementById('js-recipe-toggle');
 
 navBarToggle.addEventListener('click', function(){
   mainNav.classList.toggle('active');
 });
 bookmarkToggle.addEventListener('click', function(){
   bookmark.classList.toggle('active');
+});
+recipeToggle.addEventListener('click', function(){
+  recipe.classList.toggle('active');
 });
